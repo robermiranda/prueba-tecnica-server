f20412fc020fa93c20f3181750b771742b068b63
Rober Miranda <rob.mirandam@gmail.com>
2023-04-21 20:13:08 -0600

    Getting the bank from the file  bancos.js

--- routes/bank.js
+++ routes/bank.js
@@ -1,19 +1,14 @@
-const axios = require('axios');
 const express = require('express');
 const router = express.Router();
-const URL = 'https://dev.obtenmas.com/catom/api/challenge/banks';
+const bancos = require('./bancos');
 
 router.get('/', function (req, res, next) {
-    axios.get(URL)
-    .then(list => {
-        //res.send(`bank list size: ${list.data.length}`);
-        res.json(list.data);
-    })
-    .catch(error => {
-        const msg = 'ERROR WHILE GETTING BANK LIST';
-        console.error(msg, error.mesage);
-        res.send(msg);
-    });
+    try {
+        res.send(bancos);
+    }
+    catch (error) {
+        res.json([]);
+    }
 });
 
 module.exports = router;
